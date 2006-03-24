b29eb04231253677398d289b21daea488418fe94
Yuval Kogman <nothingmuch@woobling.org>
2006-03-24 19:47:50 +0000

    Docs

--- lib/Context/Handle.pm
+++ lib/Context/Handle.pm
@@ -185,6 +185,124 @@ Context::Handle - A convenient context propagation proxy thingy.
 This module lets you delegate to another method and return the value without
 caring about context propagation.
 
+The level of support is tied to what L<Want> does - this module tries to make
+all the distinctions Want can make fully supported, for example array
+dereference context, boolean context, etc.
+
+=head1 EXPORTS
+
+Nothing is exported by default.
+
+=over 4
+
+=item context_sensitive BLOCK
+
+This is a convenience shortcut that calls C<new>
+
+=back
+
+=head1 METHODS
+
+=head2 Regular Usage
+
+=over 4
+
+=item new $code
+
+This method invokes $code in the calling sub's context, and returns an object
+that saves the return value.
+
+=item rv_container
+
+This instance method returns the return value container object. The only useful
+methods for the RV containers is C<value>, which has a delegator anyway.
+
+=item value
+
+This returns the value from the C<rv_container>
+
+=item return
+
+This (ab)uses L<Want> to perform a double return.
+
+Saying
+
+	$rv->return;
+
+is just like
+
+	return $rv->value;
+
+=back
+
+=head2 Introspection
+
+Incidientially due to the needs of the wrapping layer this module also provides
+an OO interface to L<Want>, more or less ;-)
+
+=over 4
+
+=item bool
+
+=item void
+
+=item scalar
+
+=item list
+
+=item refarray
+
+=item refhash
+
+=item refscalar
+
+=item refobject
+
+=item refcode
+
+=item refglob
+
+All of these methods return boolean values, with respect to the 
+
+=back
+
+=head1 TODO
+
+=over 4
+
+=item *
+
+Return value arity count (this is how split knows when to stop returning values)
+
+=item *
+
+pseudoboolean context - the right side of && and the left side of || evaulate
+in boolean context, but still return a meaningful value.
+
+=item *
+
+Glob assignment context
+
+=item *
+
+Lvalue assignment
+
+=back
+
+=head1 ACKNOWLEGMENTS
+
+Robin Houston for L<Want> and lots of help by email
+
+=head1 AUTHOR
+
+Yuval Kogman <nothingmuch@woobling.org>
+
+=head1 COPYRIGHT & LICENSE
+
+	Copyright (c) 2006 the aforementioned authors. All rights
+	reserved. This program is free software; you can redistribute
+	it and/or modify it under the same terms as Perl itself.
+
 =cut
 
 
